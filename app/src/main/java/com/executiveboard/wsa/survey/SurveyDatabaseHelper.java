40f6b8143afca9698bd3262a2a52808bd77b21e9
Kenneth Cordero <kencordero@gmail.com>
2014-06-12 15:16:48 -0500

    Replaces tabs with spaces

--- app/src/main/java/com/executiveboard/wsa/survey/SurveyDatabaseHelper.java
+++ app/src/main/java/com/executiveboard/wsa/survey/SurveyDatabaseHelper.java
@@ -19,178 +19,178 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class SurveyDatabaseHelper extends SQLiteOpenHelper {
-	private static final String TAG = "SurveyDatabaseHelper";
-	private static final int DB_VERSION = 1;	
+    private static final String TAG = "SurveyDatabaseHelper";
+    private static final int DB_VERSION = 1;    
 
-	private SQLiteDatabase mDatabase;
-	private final Context mContext;
-	private final String mDbName;
-	private String mPath;
+    private SQLiteDatabase mDatabase;
+    private final Context mContext;
+    private final String mDbName;
+    private String mPath;
 
-	public SQLiteDatabase getDb() {
-		return mDatabase;
-	}
+    public SQLiteDatabase getDb() {
+        return mDatabase;
+    }
 
-	/**
-	 * Constructor
-	 * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
-	 * @param context activity or application context
+    /**
+     * Constructor
+     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
+     * @param context activity or application context
      * @param dbName filename for database
-	 */
-	public SurveyDatabaseHelper(Context context, String dbName) {		
-		super(context, dbName, null, DB_VERSION);
-		Log.i(TAG, "init");
-		mDbName = dbName;
-		mContext = context;
-		openDatabase();
-	}   
-	
-	/**
-	 * Creates a empty database on the system and rewrites it with your own database.
-	 * */
-	public void createDatabase() {
-		Log.i(TAG, "createDatabase");
-		boolean dbExists = checkDatabase();
+     */
+    public SurveyDatabaseHelper(Context context, String dbName) {        
+        super(context, dbName, null, DB_VERSION);
+        Log.i(TAG, "init");
+        mDbName = dbName;
+        mContext = context;
+        openDatabase();
+    }   
+    
+    /**
+     * Creates a empty database on the system and rewrites it with your own database.
+     * */
+    public void createDatabase() {
+        Log.i(TAG, "createDatabase");
+        boolean dbExists = checkDatabase();
 
-		if (!dbExists) {
-			// By calling this method, an empty database will be created into the default system path
-			// of your application so we are able to overwrite that database with ours.
-			getReadableDatabase();
+        if (!dbExists) {
+            // By calling this method, an empty database will be created into the default system path
+            // of your application so we are able to overwrite that database with ours.
+            getReadableDatabase();
 
-			try {
-				copyDatabase();
-			} catch (IOException e) {
-				Log.e(TAG, "Couldn't copy database", e);
-				throw new Error("Error copying database");
-			}
-		}
-	}
+            try {
+                copyDatabase();
+            } catch (IOException e) {
+                Log.e(TAG, "Couldn't copy database", e);
+                throw new Error("Error copying database");
+            }
+        }
+    }
 
-	/**
-	 * Check if the database already exist to avoid re-copying the file each time you open the application.
-	 * @return true if it exists, false if it doesn't
-	 */
-	private boolean checkDatabase() {
-		Log.i(TAG, "checkDatabase");
-		SQLiteDatabase database = null;
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDatabase() {
+        Log.i(TAG, "checkDatabase");
+        SQLiteDatabase database = null;
 
-		try {
-			database = SQLiteDatabase.openDatabase(getPath(), null, SQLiteDatabase.OPEN_READONLY);
-		} catch(SQLiteException e) {
-			Log.e(TAG, "Error while checking db");	            
-		}
+        try {
+            database = SQLiteDatabase.openDatabase(getPath(), null, SQLiteDatabase.OPEN_READONLY);
+        } catch(SQLiteException e) {
+            Log.e(TAG, "Error while checking db");                
+        }
 
-		if (database != null)
-			database.close();
+        if (database != null)
+            database.close();
 
-		return database != null;
-	}
+        return database != null;
+    }
 
-	/**
-	 * Copies your database from your local assets-folder to the just created empty database in the
-	 * system folder, from where it can be accessed and handled.
-	 * This is done by transferring bytestream.
-	 * */
-	private void copyDatabase() throws IOException {
-		Log.i(TAG, "copyDatabase");
-		//Open your local db as the input stream
-		InputStream myInput = mContext.getAssets().open(mDbName);
+    /**
+     * Copies your database from your local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transferring bytestream.
+     * */
+    private void copyDatabase() throws IOException {
+        Log.i(TAG, "copyDatabase");
+        //Open your local db as the input stream
+        InputStream myInput = mContext.getAssets().open(mDbName);
 
-		//Open the empty db as the output stream
-		OutputStream myOutput = new FileOutputStream(getPath());
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(getPath());
 
-		//transfer bytes from the inputfile to the outputfile
-		byte[] buffer = new byte[1024];
-		int length;
-		while ((length = myInput.read(buffer)) > 0) {
-			myOutput.write(buffer, 0, length);
-		}
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
 
-		//Close the streams
-		myOutput.flush();
-		myOutput.close();
-		myInput.close();
-	}
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
 
-	private String getPath() {
-		if (mPath == null) {
-			mPath = mContext.getDatabasePath(mDbName).getPath();
-			Log.i(TAG, "Path: " + mPath);
-		}
-		return mPath;
-	}
+    private String getPath() {
+        if (mPath == null) {
+            mPath = mContext.getDatabasePath(mDbName).getPath();
+            Log.i(TAG, "Path: " + mPath);
+        }
+        return mPath;
+    }
 
-	public SQLiteDatabase openDatabase() throws SQLException {
-		Log.i(TAG, "openDatabase");
-		//Open the database	      
-		if (mDatabase == null) {
-			createDatabase();
-			mDatabase = SQLiteDatabase.openDatabase(getPath(), null, SQLiteDatabase.OPEN_READONLY);
-		}
-		return mDatabase;
-	}
+    public SQLiteDatabase openDatabase() throws SQLException {
+        Log.i(TAG, "openDatabase");
+        //Open the database          
+        if (mDatabase == null) {
+            createDatabase();
+            mDatabase = SQLiteDatabase.openDatabase(getPath(), null, SQLiteDatabase.OPEN_READONLY);
+        }
+        return mDatabase;
+    }
 
-	@Override
-	public synchronized void close() {
-		Log.i(TAG, "close");
-		if (mDatabase != null)
-			mDatabase.close();
+    @Override
+    public synchronized void close() {
+        Log.i(TAG, "close");
+        if (mDatabase != null)
+            mDatabase.close();
 
-		super.close();
-	}
+        super.close();
+    }
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		Log.i(TAG, "onCreate");
-	}
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i(TAG, "onCreate");
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.i(TAG, "onUpgrade");
-	}
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i(TAG, "onUpgrade");
+    }
 
-	// public helper methods to access and get content from the database.
-	public ResponseScale getItemResponseScale(Item item) {
-		Cursor cursor = mDatabase.rawQuery("SELECT ro._id, ro.text FROM response_options ro " +
-			"JOIN response_scale_response_options rsro ON ro._id = rsro.response_option_id " +
-			"JOIN items i ON i.response_scale_id = rsro.response_scale_id " +
-			"WHERE i._id = ? ORDER BY rsro.sequence", new String[] {item.getId()});
-		ResponseScale scale = new ResponseScale();
-		if (cursor != null) {
-			try {
-				cursor.moveToFirst();
-				do {
+    // public helper methods to access and get content from the database.
+    public ResponseScale getItemResponseScale(Item item) {
+        Cursor cursor = mDatabase.rawQuery("SELECT ro._id, ro.text FROM response_options ro " +
+            "JOIN response_scale_response_options rsro ON ro._id = rsro.response_option_id " +
+            "JOIN items i ON i.response_scale_id = rsro.response_scale_id " +
+            "WHERE i._id = ? ORDER BY rsro.sequence", new String[] {item.getId()});
+        ResponseScale scale = new ResponseScale();
+        if (cursor != null) {
+            try {
+                cursor.moveToFirst();
+                do {
                     int optionId = cursor.getInt(cursor.getColumnIndex("_id"));
                     String optionText = cursor.getString(cursor.getColumnIndex("text"));
-					scale.addOption(Integer.toString(optionId), optionText);
-				} while (cursor.moveToNext());
-			} finally {
-				cursor.close();
-			}
-		}
-		return scale;
-	}
-	
-	public Survey getSurvey() {
-		Survey survey = Survey.get(mContext);
-		if (survey.getItemCount() > 0) // survey already populated
-			return survey;
-		Cursor cursor = mDatabase.rawQuery("SELECT _id, text FROM items ORDER BY RANDOM()",
-				null);
-		if (cursor != null) {
-			try {
-				cursor.moveToFirst();
-				do {
-					int itemId = cursor.getInt(cursor.getColumnIndex("_id"));
-					String itemText = cursor.getString(cursor.getColumnIndex("text"));
-					survey.addItem(Integer.toString(itemId), itemText);
-				} while (cursor.moveToNext());
-			} finally {
-				cursor.close();
-			}
-		}		
-		return survey;
-	}
+                    scale.addOption(Integer.toString(optionId), optionText);
+                } while (cursor.moveToNext());
+            } finally {
+                cursor.close();
+            }
+        }
+        return scale;
+    }
+    
+    public Survey getSurvey() {
+        Survey survey = Survey.get(mContext);
+        if (survey.getItemCount() > 0) // survey already populated
+            return survey;
+        Cursor cursor = mDatabase.rawQuery("SELECT _id, text FROM items ORDER BY RANDOM()",
+                null);
+        if (cursor != null) {
+            try {
+                cursor.moveToFirst();
+                do {
+                    int itemId = cursor.getInt(cursor.getColumnIndex("_id"));
+                    String itemText = cursor.getString(cursor.getColumnIndex("text"));
+                    survey.addItem(Integer.toString(itemId), itemText);
+                } while (cursor.moveToNext());
+            } finally {
+                cursor.close();
+            }
+        }        
+        return survey;
+    }
 
     public void setSession() {
         ContentValues cv = new ContentValues();
--- app/src/main/java/com/executiveboard/wsa/survey/SurveyItemPagerActivity.java
+++ app/src/main/java/com/executiveboard/wsa/survey/SurveyItemPagerActivity.java
@@ -14,57 +14,57 @@ import com.executiveboard.wsa.survey.models.Item;
 import com.executiveboard.wsa.survey.models.Survey;
 
 public class SurveyItemPagerActivity extends FragmentActivity implements SurveyItemFragment.Callbacks {
-	private static final String TAG = "SurveyItemPagerActivity";
-	private static final String DB_NAME = "survey.db3";
-	
-	private NonSwipeableViewPager mViewPager;
-	private SurveyDatabaseHelper mHelper;
-	private Survey mSurvey;
-	private int mIdx;
-	
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
-		mViewPager = new NonSwipeableViewPager(this);
-		mViewPager.setId(R.id.viewPager);
-		setContentView(mViewPager);
-		
-		mHelper = new SurveyDatabaseHelper(this, DB_NAME);
-		mHelper.openDatabase();
-		
-		mSurvey = mHelper.getSurvey();
-		setTitle();
-				
-		FragmentManager fm = getSupportFragmentManager();
-		mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
-			public Fragment getItem(int idx) {				
-				boolean isFirst = (idx == 0);
-				boolean isLast = (idx == getCount() - 1);				
-				String itemId = mSurvey.getItem(idx).getId();
-				Log.i(TAG, "Load item #" + idx);
-				return SurveyItemFragment.newInstance(itemId, isFirst, isLast);
-			}
+    private static final String TAG = "SurveyItemPagerActivity";
+    private static final String DB_NAME = "survey.db3";
+    
+    private NonSwipeableViewPager mViewPager;
+    private SurveyDatabaseHelper mHelper;
+    private Survey mSurvey;
+    private int mIdx;
+    
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+        mViewPager = new NonSwipeableViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+        
+        mHelper = new SurveyDatabaseHelper(this, DB_NAME);
+        mHelper.openDatabase();
+        
+        mSurvey = mHelper.getSurvey();
+        setTitle();
+                
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            public Fragment getItem(int idx) {                
+                boolean isFirst = (idx == 0);
+                boolean isLast = (idx == getCount() - 1);                
+                String itemId = mSurvey.getItem(idx).getId();
+                Log.i(TAG, "Load item #" + idx);
+                return SurveyItemFragment.newInstance(itemId, isFirst, isLast);
+            }
 
-			public int getCount() {
-				return mSurvey.getItemCount();				
-			}
-		});
-	}
-	
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setTitle() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-			getActionBar().setTitle((mIdx+1) + " of " + mSurvey.getItemCount());
-	}
+            public int getCount() {
+                return mSurvey.getItemCount();                
+            }
+        });
+    }
+    
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setTitle() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+            getActionBar().setTitle((mIdx+1) + " of " + mSurvey.getItemCount());
+    }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		Log.i(TAG, "onPause");
-		mHelper.close();
-	}
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+        mHelper.close();
+    }
 
     @Override
     public void onResume() {
@@ -73,11 +73,11 @@ public class SurveyItemPagerActivity extends FragmentActivity implements SurveyI
         mHelper.getDb();
     }
 
-	// Fragment callback interface methods
-	@Override
-	public void onLoadItem(Item item) {
-		item.setResponseScale(mHelper.getItemResponseScale(item));		
-	}
+    // Fragment callback interface methods
+    @Override
+    public void onLoadItem(Item item) {
+        item.setResponseScale(mHelper.getItemResponseScale(item));        
+    }
 
     @Override
     public void onSubmit() {
@@ -86,16 +86,16 @@ public class SurveyItemPagerActivity extends FragmentActivity implements SurveyI
     }
 
     @Override
-	public void onNext() {
-		mViewPager.setCurrentItem(++mIdx, true);
-		setTitle();
-	}
+    public void onNext() {
+        mViewPager.setCurrentItem(++mIdx, true);
+        setTitle();
+    }
 
-	@Override
-	public void onPrevious() {
-		mViewPager.setCurrentItem(--mIdx, true);
-		setTitle();
-	}
+    @Override
+    public void onPrevious() {
+        mViewPager.setCurrentItem(--mIdx, true);
+        setTitle();
+    }
 
 
 }
